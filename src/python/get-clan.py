4695db63791bd5371f1e89c6d797ab820b4a2967
cmkennedy20 <cmkennedy20@gmail.com>
2023-09-02 19:32:46 -0400

    Adding basic fetch functionality

---
+++ src/python/get-clan.py
@@ -0,0 +1,49 @@
+import requests
+import os
+
+CLAN_TAG = "%239RJOYLR9"
+
+# Define the headers with the API token.
+headers = {
+    "Authorization": f"Bearer {os.environ.get('API_TOKEN_1')}"
+}
+def get_clan():
+    API_URL = f"https://api.clashofclans.com/v1/clans/{CLAN_TAG}"
+    try:
+        # Make an HTTP GET request to the Clash of Clans API.
+        response = requests.get(API_URL, headers=headers)
+        if response.status_code == 200:
+            clan_data = response.json()
+            return clan_data
+        else:
+            print(f"Error: {response.status_code} - {response.text}")
+    except requests.exceptions.RequestException as e:
+        print(f"Request error: {e}")
+
+def get_members(data):
+    API_URL = f"https://api.clashofclans.com/v1/clans/{CLAN_TAG}/members"
+    try:
+        # Make an HTTP GET request to the Clash of Clans API.
+        response = requests.get(API_URL, headers=headers)
+        if response.status_code == 200:
+            clan_data = response.json()
+            return clan_data
+        else:
+            print(f"Error: {response.status_code} - {response.text}")
+    except requests.exceptions.RequestException as e:
+        print(f"Request error: {e}")
+
+
+def get_war_history():
+    API_URL = f"https://api.clashofclans.com/v1/clans/{CLAN_TAG}/warlog"
+    try:
+        # Make an HTTP GET request to the Clash of Clans API.
+        response = requests.get(API_URL, headers=headers)
+        if response.status_code == 200:
+            clan_data = response.json()
+            return clan_data
+        else:
+            print(f"Error: {response.status_code} - {response.text}")
+    except requests.exceptions.RequestException as e:
+        print(f"Request error: {e}")
+
--- tools/docker/python.Dockerfile
+++ tools/docker/python.Dockerfile
@@ -1,2 +1,6 @@
-FROM 3.12.0rc1-alpine3.18
+FROM python:3.12.0rc1-alpine3.18
+RUN apk add bash
 RUN mkdir -p /opt/python_home/
+RUN pip install requests
+
+WORKDIR /opt/python_home/
