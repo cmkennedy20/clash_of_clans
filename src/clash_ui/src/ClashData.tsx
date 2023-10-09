261c956501039f7e3dbc849a8684bb05d23dc247
cmkennedy20 <cmkennedy20@gmail.com>
2023-10-08 21:17:45 -0400

    Why is it not working?

--- src/clash_ui/src/ClashData.tsx
+++ src/clash_ui/src/ClashData.tsx
@@ -2,41 +2,56 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 interface DataItem {
-  id: number;
-  name: string;
+  message: Blob
   // Define other properties as needed
 }
+const base_url = 'http://localhost:5000'
 
 function DataDisplay() {
-  const [data, setData] = useState<DataItem[]>([]);
-  const [loading, setLoading] = useState(true);
+  const [userData, setUserData] = useState<DataItem[]>([]);
+  const getClanMembers = async () => {
+      const response = await axios.get(`${base_url}/clan-members`);
+      setUserData(response.data);
+    };
 
-  useEffect(() => {
-    // Replace 'API_ENDPOINT' with the actual API endpoint you want to fetch data from
-    axios.get<DataItem[]>('http://localhost:5000/clan-members')
-      .then((response:any) => {
-        setData(response.data);
-        setLoading(false);
-      })
-      .catch((error:any) => {
-        console.error('Error fetching data:', error);
-        setLoading(false);
-      });
-  }, []);
+    useEffect(() => {
+        getClanMembers();
+    }, []);
+//   useEffect(() => {
+//     // Replace 'API_ENDPOINT' with the actual API endpoint you want to fetch data from
+//     axios.get<DataItem[]>('http://localhost:5000/clan-members')
+//       .then((response:any) => {
+//         setData(response.data);
+//         setLoading(false);
+//       })
+//       .catch((error:any) => {
+//         console.error('Error fetching data:', error);
+//         setLoading(fals  e);
+//       });
+//   }, []);
 
-  if (loading) {
-    return <div>Loading...</div>;
-  }
+//   if (loading) {
+//     return <div>Loading...</div>;
+//   }
 
   return (
-    <div>
-      <h1>Data Display</h1>
-      <ul>
-        {data.map(item => (
-          <li key={item.id}>{item.name}</li>
-        ))}
-      </ul>
+
+    <div className="App">
+    <header className="App-header">
+      <h2>GitHub User Data</h2>
+    </header>
+    <div className="user-container">
+      <h5 className="info-item">{userData.message}</h5>
     </div>
+  </div>    // <div>
+    //   <h1>Data Display</h1>
+    //   <ul>
+    //     {/* {userData} */}
+    //     {/* {userData.map(item => (
+    //       <li key={item.id}>{item.name}</li>
+    //     ))} */}
+    //   </ul>
+    // </div>
   );
 }
 
