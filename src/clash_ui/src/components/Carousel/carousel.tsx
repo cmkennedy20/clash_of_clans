92635e5a277a80a7cbc612876bef240404220850
cmkennedy20 <cmkennedy20@gmail.com>
2023-11-30 20:59:34 -0500

    Add an image component (this isn't right)

--- src/clash_ui/src/components/Carousel/carousel.tsx
+++ src/clash_ui/src/components/Carousel/carousel.tsx
@@ -1,6 +1,6 @@
 import { SetStateAction, useState } from 'react';
 import Carousel from 'react-bootstrap/Carousel';
-import ExampleCarouselImage from 'public/clash-logo.jpeg';
+import FluidExample from '../../components/Images/clashlogo';
 
 function ControlledCarousel() {
   const [index, setIndex] = useState(0);
@@ -12,21 +12,21 @@ function ControlledCarousel() {
   return (
     <Carousel activeIndex={index} onSelect={handleSelect}>
       <Carousel.Item>
-        <ExampleCarouselImage/>
+        <FluidExample text="test"/>
         <Carousel.Caption>
           <h3>First slide label</h3>
           <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
-        <ExampleCarouselImage />
+        <FluidExample text="test"/>
         <Carousel.Caption>
           <h3>Second slide label</h3>
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
-        <ExampleCarouselImage  />
+        <FluidExample text="test" />
         <Carousel.Caption>
           <h3>Third slide label</h3>
           <p>
---
+++ src/clash_ui/src/components/Images/clashlogo.tsx
@@ -0,0 +1,7 @@
+import Image from 'react-bootstrap/Image';
+
+function FluidExample() {
+  return <Image src="../../../public/clashlogo.jpg" fluid />;
+}
+
+export default FluidExample;
