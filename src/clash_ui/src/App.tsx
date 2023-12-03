ee02057f6d3ff19deac893ebe34e0e2d97206985
cmkennedy20 <cmkennedy20@gmail.com>
2023-12-03 10:50:22 -0500

    Navbar is created - need to get span working properly

--- src/clash_ui/src/App.tsx
+++ src/clash_ui/src/App.tsx
@@ -1,13 +1,11 @@
 import './App.css'
 import HeaderNavbar from './components/Navbar/navbar';
-import ControlledCarousel from './components/Carousel/carousel';
 import 'bootstrap/dist/css/bootstrap.min.css';
 function App() {
 
   return (
     <>
-      <ControlledCarousel />
-      <HeaderNavbar />
+      <span> <HeaderNavbar /></span>
     </>
   )
 }
--- src/clash_ui/src/components/Images/clashlogo.tsx
+++
@@ -1,7 +0,0 @@
-import Image from 'react-bootstrap/Image';
-
-function FluidExample() {
-  return <Image src="../../../public/clashlogo.jpg" fluid />;
-}
-
-export default FluidExample;
